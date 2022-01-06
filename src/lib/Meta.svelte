f2c0c9178f7f8a79293745a71b5ef95d630da6d2
Heba <heba.n.elshimy@gmail.com>
2022-01-07 03:11:20 +0400

    Basic setup working

---
+++ src/lib/Meta.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+    <meta charset="utf-8" />
+    <meta name="description" content="This is my personal academic portfolio site and blog where I share what I'm reading or learning. To go to the live website follow this link: https://www.macs.hw.ac.uk/~he12" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Heba El-Shimy" />
+    <meta name="keywords" content="data, data science, machine learning, AI, artificial intelligence, deep learning, portfolio, research, academic, blog" />
+    <meta property="og:site_name" content="Heba El-Shimy Academic Site and Blog" />
+    <meta property="og:url" content="https://www.macs.hw.ac.uk/~he12" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="| Data Scientist, Researcher and Lecturer" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="content-language" content="en" />
+    <meta http-equiv="twitter-username" content="@heba_el_shimy" />
+    <meta http-equiv="title-template" content="%s ∙ Heba El-Shimy" />
+    <link rel="icon" href="/favicon.png" />
+    <link rel="canonical" href="https://www.macs.hw.ac.uk/~he12" />
+    <link rel="alternate" hreflang="x-default" href="https://www.macs.hw.ac.uk/~he12">
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+</svelte:head>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+
+<script>
+    import Meta from "$lib/Meta.svelte";
+    import '../app.css';
+</script>
+
+<Meta />
+<div>This should be a Header</div>
+
+<slot></slot>
